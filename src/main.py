bf9bc108ee822052891d962eccfafa2c32070482
Tilda Engberg <tildaengberg@client213-79.wireless.umu.se>
2023-02-23 13:46:51 +0100

    minifix

--- src/main.py
+++ src/main.py
@@ -17,59 +17,37 @@ data_2018 = data_2018.dropna()
 data_2019 = data_2019.dropna()
 
 # clean tables from unnecessary cloumns
-data_2015.drop(["Region", "Standard Error", "Family", "Dystopia Residual"], axis=1, inplace=True)
-data_2016.drop(["Region", "Lower Confidence Interval", "Upper Confidence Interval", "Family", "Dystopia Residual"], axis=1, inplace=True)
-data_2017.drop(["Whisker.high", "Whisker.low", "Family", "Dystopia.Residual"], axis=1, inplace=True)
+data_2015.drop(["Region", "Standard Error", "Family",
+               "Dystopia Residual"], axis=1, inplace=True)
+data_2016.drop(["Region", "Lower Confidence Interval", "Upper Confidence Interval",
+               "Family", "Dystopia Residual"], axis=1, inplace=True)
+data_2017.drop(["Whisker.high", "Whisker.low", "Family",
+               "Dystopia.Residual"], axis=1, inplace=True)
 data_2018.drop(["Social support"], axis=1, inplace=True)
 data_2019.drop(["Social support"], axis=1, inplace=True)
 
 # rename column labels
-data_2015 = data_2015.rename(columns = {'Happiness Rank': 'Rank', 'Happiness Score': 'Score', 'Economy (GDP per Capita)': 'Economy', 'Health (Life Expectancy)': 'Health', 'Trust (Government Corruption)': 'Corruption' }) 
-data_2016 = data_2016.rename(columns = {'Happiness Rank': 'Rank', 'Happiness Score': 'Score', 'Economy (GDP per Capita)': 'Economy', 'Health (Life Expectancy)': 'Health', 'Trust (Government Corruption)': 'Corruption' }) 
-data_2017 = data_2017.rename(columns = {'Happiness.Rank': 'Rank', 'Happiness.Score': 'Score', 'Economy..GDP.per.Capita.': 'Economy', 'Health..Life.Expectancy.': 'Health', 'Trust..Government.Corruption.': 'Corruption' }) 
-data_2018 = data_2018.rename(columns = {'Overall rank': 'Rank', 'Country or region': 'Country', 'GDP per capita': 'Economy', 'Healthy life expectancy': 'Health', 'Freedom to make life choices': 'Freedom', 'Perceptions of corruption':  'Corruption'}) 
-data_2019 = data_2019.rename(columns = {'Overall rank': 'Rank', 'Country or region': 'Country', 'GDP per capita': 'Economy', 'Healthy life expectancy': 'Health', 'Freedom to make life choices': 'Freedom', 'Perceptions of corruption': 'Corruption' }) 
+data_2015 = data_2015.rename(columns={'Happiness Rank': 'Rank', 'Happiness Score': 'Score', 'Economy (GDP per Capita)': 'Economy',
+                             'Health (Life Expectancy)': 'Health', 'Trust (Government Corruption)': 'Corruption'})
+data_2016 = data_2016.rename(columns={'Happiness Rank': 'Rank', 'Happiness Score': 'Score', 'Economy (GDP per Capita)': 'Economy',
+                             'Health (Life Expectancy)': 'Health', 'Trust (Government Corruption)': 'Corruption'})
+data_2017 = data_2017.rename(columns={'Happiness.Rank': 'Rank', 'Happiness.Score': 'Score', 'Economy..GDP.per.Capita.': 'Economy',
+                             'Health..Life.Expectancy.': 'Health', 'Trust..Government.Corruption.': 'Corruption'})
+data_2018 = data_2018.rename(columns={'Overall rank': 'Rank', 'Country or region': 'Country', 'GDP per capita': 'Economy',
+                             'Healthy life expectancy': 'Health', 'Freedom to make life choices': 'Freedom', 'Perceptions of corruption':  'Corruption'})
+data_2019 = data_2019.rename(columns={'Overall rank': 'Rank', 'Country or region': 'Country', 'GDP per capita': 'Economy',
+                             'Healthy life expectancy': 'Health', 'Freedom to make life choices': 'Freedom', 'Perceptions of corruption': 'Corruption'})
 
 # sort columns
-column_titles = ["Country", "Rank", "Score", "Economy", "Health", "Freedom", "Generosity", "Corruption"]
+column_titles = ["Country", "Rank", "Score", "Economy",
+                 "Health", "Freedom", "Generosity", "Corruption"]
 data_2015 = data_2015.reindex(columns=column_titles)
 data_2016 = data_2016.reindex(columns=column_titles)
 data_2017 = data_2017.reindex(columns=column_titles)
 data_2018 = data_2018.reindex(columns=column_titles)
 data_2019 = data_2019.reindex(columns=column_titles)
 
-print("")
-
-print("2015:")
-print(len(data_2015.columns))
-print(data_2015.columns.values.tolist())
-print("")
-
-print("2016:")
-print(len(data_2016.columns))
-print(data_2016.columns.values.tolist())
-print("")
-
-print("2017:")
-print(len(data_2017.columns))
-print(data_2017.columns.values.tolist())
-print("")
-
-print("2018:")
-print(len(data_2018.columns))
-print(data_2018.columns.values.tolist())
-print("")
-
-print("2019:")
-print(len(data_2019.columns))
-print(data_2019.columns.values.tolist())
-print("")
-
-print("-----------")
-
 print(data_2019)
-
-
 
 # plot bar graph
 economy_mean_2019 = data_2019["Economy"].mean()
@@ -79,7 +57,8 @@ generosity_mean_2019 = data_2019["Generosity"].mean()
 corruption_mean_2019 = data_2019["Corruption"].mean()
 
 names = ['economy', 'health', 'freedom', 'generosity', 'corruption']
-values = [economy_mean_2019, health_mean_2019, freedom_mean_2019, generosity_mean_2019, corruption_mean_2019]
+values = [economy_mean_2019, health_mean_2019, freedom_mean_2019,
+          generosity_mean_2019, corruption_mean_2019]
 
 plt.bar(names, values)
-plt.show() 
+plt.show()
